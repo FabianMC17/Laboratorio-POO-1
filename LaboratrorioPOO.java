0f3c933d5c5f2578092a6e442d11f4839e816379
FabianMC17 <fabian.maquen@usil.pe>
2023-08-23 14:35:12 -0500

    LaboratorioDeGuia

---
+++ LaboratrorioPOO.java
@@ -0,0 +1,26 @@
+public class LaboratrorioPOO {
+    public static void main(String[] args) {
+
+        //variable numero entero
+		// int valor =10;
+        // double numPrincipal = 23.4;
+        
+		// System.out.println("Hola");
+        // System.out.println(valor);
+        // System.out.println(numPrincipal);
+
+        int num1 = 1000000000;
+        int num2 = 2000000000;
+        int resultado = num1 + num2;
+
+        System.out.println(resultado);
+
+        int division = 1/0;
+        System.out.println(division);
+        
+        int modulo = 7%-3;
+        System.out.println(modulo);
+        
+
+    }
+}
